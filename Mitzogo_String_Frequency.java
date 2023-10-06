d05b1b1889bf9d376b3638ab584cc0d1ce14eac0
Venkat1506ak <139452463+Venkat1506ak@users.noreply.github.com>
2023-10-06 12:36:21 +0530

    Add files via upload

---
+++ Mitzogo_String_Frequency.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+public class Mitsogo {
+
+    
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Chemical name:");
+        String sam=sc.nextLine();
+        int count=0,val=0;
+        String[] sri=sam.split(" ");
+        for(int i=0;i<sri.length-1;i++){
+            StringBuffer s=new StringBuffer(sri[i]);
+            for(int j=i+1;j<sri.length;j++){
+                StringBuffer a=new StringBuffer(sri[j]);
+                for(int k=0;k<s.length();k++){
+                    for(int l=0;l<a.length();l++){
+                        if(s.charAt(k)==a.charAt(l)){
+                            val++;
+                        }
+                    }
+                }
+                if(val>=s.length() || val>=a.length()){
+                    count++;
+                }
+                val=0;
+            }
+        }
+        System.out.println("Explotions:"+count);
+    }
+    
+}
