82b087fbba273c39576d8592c7451fafb0488198
Venkat1506ak <139452463+Venkat1506ak@users.noreply.github.com>
2023-11-01 13:40:20 +0530

    Updated N_Binary_with_no_consecutive_1's.java

--- N_Binary_with_no_consecutive_1's.java
+++ N_Binary_with_no_consecutive_1's.java
@@ -10,6 +10,20 @@ public class binary {
         int e=(int) Math.pow(2, n);
         for(int i=0;i<e;i++){
             String m=toBinaryString(i);
+            String temp = new String("");
+
+                // First logic
+                // if(m.length() < n){
+                //     for(int j= m.length(); j < n; j++){
+                //         temp = temp + '0';
+                //     }
+                // }
+
+                // Second logic
+                // while(temp.length() < n - m.length()){
+                //     temp = temp + '0';
+                // }
+            
             if(m.contains("11")){       
             }
             else{
